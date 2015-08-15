5c1b7dd1bab3c2d55688d47a9a69515e2ea14c33
Todd Ruhlen <rhythmtome@yahoo.com>
2015-08-15 12:04:23 -0700

    Add validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,15 @@
 class Movie < ActiveRecord::Base
 
+  validates :title, :released_on, :duration, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_file_name, allow_blank: true, format: {
+    with:    /\w+\.(gif|jpg|png)\z/i,
+    message: "must reference a GIF, JPG, or PNG image"
+  }
+  RATINGS = %W(G PG PG-13 R NC-17 NR)
+  validates :rating, inclusion: { in: RATINGS }
+
   def flop?
     total_gross.blank? || total_gross < 50_000_000
   end
--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -6,6 +6,7 @@ describe "Creating a movie" do
 
     fill_in "Title", with: "New Movie Title"
     fill_in "Description", with: "Superheroes saving the world from villians."
+    select "PG-13", from: "movie_rating"
     fill_in "Total gross", with: "75000000"
     select (Time.now.year - 1).to_s, :from => "movie_released_on_1i"
     fill_in "Cast", with: "The award-winning cast"
