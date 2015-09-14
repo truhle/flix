c08d8b1d82277317af129a1bac2cccb7c2c0f18d
Todd Ruhlen <rhythmtome@yahoo.com>
2015-09-14 12:52:27 -0400

    Fix specs and make reviews rely on sign in

--- spec/features/create_review_spec.rb
+++ spec/features/create_review_spec.rb
@@ -1,4 +1,11 @@
 describe "Creating a new review" do
+
+  before do
+    @user = User.create!(user_attributes)
+
+    sign_in(@user)
+  end
+
   it 'saves the review' do
     movie = Movie.create(movie_attributes)
 
@@ -8,7 +15,6 @@ describe "Creating a new review" do
 
     expect(current_path).to eq(new_movie_review_path(movie))
 
-    fill_in "Name", with: "Roger Ebert"
     fill_in "Location", with: "Chicago, IL"
     choose "review_stars_3"
     fill_in "Comment", with: "I laughed, I cried, I spilled my popcorn!"
@@ -18,6 +24,7 @@ describe "Creating a new review" do
     expect(current_path).to eq(movie_reviews_path(movie))
 
     expect(page).to have_text("Thanks for your review!")
+    expect(page).to have_text(@user.name)
   end
 
   it 'does not save the movie review if it is invalid' do
