9a73c8fecfc072f4e3bdeadfe080d6fd88e0a257
Todd Ruhlen <rhythmtome@yahoo.com>
2015-09-15 23:32:55 -0400

    Move favorites, genres, and fans to the sidebar in show pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def page_title
+    if content_for?(:title)
+      "Flix - #{content_for(:title)}"
+    else
+      "Flix"
+    end
+  end
 end
--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -34,4 +34,29 @@ describe "Viewing a movie page" do
     expect(page).to have_text("Flop!")
   end
 
+  it "shows the movie's fans and genres in the sidebar" do
+    movie = Movie.create!(movie_attributes)
+
+    user = User.create!(user_attributes)
+    movie.fans << user
+
+    genre = Genre.create!(name: "Action")
+    movie.genres << genre
+
+    visit movie_url(movie)
+
+    within("aside#sidebar") do
+      expect(page).to have_text(user.name)
+      expect(page).to have_text(genre.name)
+    end
+  end
+
+  it "shows a page title, including the movie title" do
+    movie = Movie.create!(movie_attributes)
+
+    visit movie_url(movie)
+
+    expect(page).to have_title("Flix - #{movie.title}")
+  end
+
 end
